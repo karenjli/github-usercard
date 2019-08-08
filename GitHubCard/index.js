b800bfc618e1f8591ab98866439e3b3c94b81813
karenjli <karenjy.li@gmail.com>
2019-08-08 17:06:30 -0500

    6 cards added

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,10 +2,44 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-axios.get('https://api.github.com/users/karenjli')
-// .then ((response) => {
-//   console.log(response)
-// })
+const karen = axios.get('https://api.github.com/users/karenjli')
+.then ((response) => {
+  //console.log(response);
+  let cardBox = document.querySelector ('.cards');
+  let newCard = new Card(response.data)
+   cardBox.appendChild(newCard)
+  })
+  .catch((err) => {
+    console.log(err);
+});
+
+
+
+function call (name)  {
+  let gituser = axios.get (`https://api.github.com/users/${name}`)
+  .then ((response) => {
+    //console.log(response);
+    let cardBox = document.querySelector ('.cards');
+    let newCard = new Card(response.data)
+     cardBox.appendChild(newCard)
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
+
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
+
+followersArray.forEach (item => {
+  call(item);
+})
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -28,7 +62,7 @@ axios.get('https://api.github.com/users/karenjli')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -57,7 +91,7 @@ function Card (user) {
 
   //img
   let userImg = document.createElement('img');
-  userImg.src = user.avatar.url;
+  userImg.src = user.avatar_url;
 
   //card-info
   let userInfo = document.createElement('div');
@@ -66,6 +100,7 @@ function Card (user) {
   //name
   let userName = document.createElement('h3');
   userName.classList.add('name');
+  userName.textContent = user.name;
   
   //username
   let userHandle = document.createElement('p');
@@ -74,44 +109,52 @@ function Card (user) {
 
   //location
   let userLocation = document.createElement('p')
-  userLocation.textContent = user.location;
+  userLocation.textContent = `Location: ${user.location}`;
 
   //Profile Link
   let userProfile = document.createElement('p')
   let profileLink = document.createElement('a');
+  profileLink.href = user.html_url;
   profileLink.textContent = user.html_url;
-
+  userProfile.textContent = `Profile: `;
+  
   //Followers
   let userFollowers = document.createElement('p')
-  userFollowers.textContent = user.followers;
+  userFollowers.textContent = `Follwers: ${user.followers}`;
 
   //Following
   let userFollowing = document.createElement('p')
-  userFollowing.textContent = user.following;
+  userFollowing.textContent = `Following: ${user.following}`;
 
   //Bio
   let userBio = document.createElement('p');
-  userBio.textContent = user.bio;
+  userBio.textContent = `Bio: ${user.bio}`;
 
   //AppendChild
-  userCard.appendChild ('userImg');
-  userCard.appendChild('userInfo');
-  userInfo.appendChild('userName');
-  userInfo.appendChild('userHandle');
-  userInfo.appendChild('userLocation');
-  userInfo.appendChild('userProfile');
-  userInfo.appendChild('userFollowers');
-  userInfo.appendChild('userFollowing');
-  userInfo.appendChild('userBio');
-  userProfile.appendChild('profileLink');
+  userCard.appendChild (userImg);
+  userCard.appendChild(userInfo);
+  userInfo.appendChild(userName);
+  userInfo.appendChild(userHandle);
+  userInfo.appendChild(userLocation);
+  userInfo.appendChild(userProfile);
+  userInfo.appendChild(userFollowers);
+  userInfo.appendChild(userFollowing);
+  userInfo.appendChild(userBio);
+  userProfile.appendChild(profileLink);
 
+  //add to cards
+  
+  //cardBox.appendChild(userCard);
 
+  return userCard;
 }
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+// Card(karen);
+// Card(dan);
+// Card(dustin);
+// Card(justsml);
+// Card(luishrd);
+// Card(bigknell);
+
+
+
